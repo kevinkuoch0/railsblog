e2c2ef1c87288bca59a046ae96fed243acc3c28d
kevinkuoch0 <kevinkuoch0@gmail.com>
2015-10-25 21:51:08 -0400

    initial commit

---
+++ kk_microblog/app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.create(params[:user])
+    redirect_to @user
+  end
+
+  def update
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  # def profile
+  #   @users = User.where(username: params[:username])
+  # end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
